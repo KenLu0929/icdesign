4fb3bc639aae5a88152b0f5f68d8ee0556426236
mrcaelumn <caelumnproperty@gmail.com>
2021-07-18 21:14:44 +0800

    add email function

---
+++ icdesign/utils.py
@@ -0,0 +1,23 @@
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from global_variables import ADMIN_EMAIL
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def send_email(subject, message, recipient_list):
+
+    if subject and message and recipient_list:
+        try:
+            send_mail(subject, message, ADMIN_EMAIL, recipient_list)
+        except BadHeaderError:
+            logger.error('Invalid header found.')
+            return False
+        return True
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.f
+        logger.error('Make sure all fields are entered and valid.')
+        return False
