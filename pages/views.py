f7d6061c597830d400d07b21ce9cbe49d925a9c6
Emily Liu <emily16928@gamil.com>
2021-09-29 14:51:56 +0800

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -611,3 +611,13 @@ def download_file_User_case_diagram(request):
     response = HttpResponse(fl, content_type=mime_type)
     response['Content-Disposition'] = "attachment; filename=%s" % filename
     return response
+    
+def download_file_Test_Seating_Plan(request):
+    # fill these variables with real values
+    fl_path = 'pages/Test_Seating_Plan.pdf'
+    filename = 'Test_Seating_Plan.pdf'
+    fl = open(fl_path, 'rb')
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(fl, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
